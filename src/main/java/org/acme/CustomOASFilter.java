527139216f6b3044552ffafdcf3c0b2cb54ddac4
Susan Mustafa <susan.mustafa@consultant.volvo.com>
2022-11-21 13:52:45 +0100

    Example of how to update schema ref

--- src/main/java/org/acme/CustomOASFilter.java
+++ src/main/java/org/acme/CustomOASFilter.java
@@ -9,12 +9,14 @@ import org.eclipse.microprofile.openapi.models.OpenAPI;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.eclipse.microprofile.openapi.models.examples.Example;
 
 import io.quarkus.logging.Log;
+import org.eclipse.microprofile.openapi.models.media.Schema;
 
 public class CustomOASFilter implements OASFilter {
 
@@ -30,6 +32,9 @@ public class CustomOASFilter implements OASFilter {
         }
 
         generateExamples().forEach(openAPI.getComponents()::addExample);
+
+        //Update REF Programmatically
+        updateHealthStatusRef(openAPI.getComponents());
     }
 
     Map<String, Example> generateExamples() {
@@ -43,13 +48,9 @@ public class CustomOASFilter implements OASFilter {
             String userJSON = new String(loader.getResourceAsStream("user.json").readAllBytes(), StandardCharsets.UTF_8);
             String customerJson = new String(loader.getResourceAsStream("customer.json").readAllBytes(), StandardCharsets.UTF_8);
 
-            Example userExample = OASFactory.createExample()
-                                            .description("User JSON Example Description")
-                                            .value(objectMapper.readValue(userJSON, ObjectNode.class));
+            Example userExample = OASFactory.createExample().description("User JSON Example Description").value(objectMapper.readValue(userJSON, ObjectNode.class));
 
-            Example customerExample = OASFactory.createExample()
-                                                .description("Customer JSON Example Description")
-                                                .value(objectMapper.readValue(customerJson, ObjectNode.class));
+            Example customerExample = OASFactory.createExample().description("Customer JSON Example Description").value(objectMapper.readValue(customerJson, ObjectNode.class));
 
             examples.put("userExample", userExample);
             examples.put("customerExample", customerExample);
@@ -60,4 +61,17 @@ public class CustomOASFilter implements OASFilter {
         return examples;
     }
 
+    void updateHealthStatusRef(Components components) {
+
+        components.getSchemas().forEach((schemaName, schemaObject) -> {
+
+            if (schemaName.equalsIgnoreCase("HealthCheckResponse")) {
+
+                Map<String, Schema> modifiableMapCopy = new HashMap<>(schemaObject.getProperties());
+                modifiableMapCopy.put("status", OASFactory.createSchema().title("My Custom Title Here").description("wheeeeeeeeeeeeeeeeeeeeeee").type(Schema.SchemaType.OBJECT).properties(Map.of("data", OASFactory.createSchema().description("Information of the service. If the service is down, this holds the information of why it is failed.").type(Schema.SchemaType.OBJECT))));
+
+                schemaObject.setProperties(modifiableMapCopy);
+            }
+        });
+    }
 }
